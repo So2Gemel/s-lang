9f7c13a15185beb59500f9580a585ba7dc1282b7
So2Gemel <90792015+So2Gemel@users.noreply.github.com>
2025-07-19 14:25:53 +0300

    conditions.s

---
+++ examples/basics/conditions.s
@@ -0,0 +1,10 @@
+func main() {
+    let score: int = 85;
+    if score >= 90 {
+        print("ممتاز");
+    } else if score >= 70 {
+        print("جيد");
+    } else {
+        print("راسب");
+    }
+}
